ced0ccab4fb1f5b7c336ad28b0fcea59f591476c
MD SLAUDDIN <88940044+Mdslauddin@users.noreply.github.com>
2026-09-16 13:28:53 +0530

    update

---
+++ frontend/script.js
@@ -0,0 +1,21 @@
+console.log("MindMap application started");
+
+const nodes = document.querySelectorAll(".node");
+
+nodes.forEach(function(node) {
+
+    node.addEventListener("click", function() {
+
+            console.log("Clicked:", node.innerText);
+
+                });
+
+                });console.log("MindMap application started");
+
+const nodes = document.querySelectorAll(".node");
+
+nodes.forEach(function(node) {
+
+    node.addEventListener("click", function() {
+
+        console.log("Clicked:", node.innerT
